3b095e26dceb45beadeda99169e3ba7ba0d7730d
ErikHolstAudio <69846011+ErikHolstAudio@users.noreply.github.com>
2024-02-02 17:05:10 +0100

    finished video. Need to add content

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,24 @@ menuIcon.classList.remove("bx-x");
 navbar.classList.remove("active");
 
 };
+
+/*===========dark light mode=============*/
+let darkModeIcon = document.querySelector("#darkMode-icon");
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle("bx-sun");
+    document.body.classList.toggle("dark-mode")
+}
+
+/*===========scroll reveal=============*/
+ScrollReveal({
+    reset: true,
+    distance: "80p", 
+    duration: 2000, 
+    delay: 200
+});
+
+ScrollReveal().reveal(".home-content, .heading", {origin: "top"});
+ScrollReveal().reveal(".home-img img, .services-container, .portfolio-box, .contact form", {origin: "bottom"});
+ScrollReveal().reveal(".home-content h1, .about-img img", {origin: "left"});
+ScrollReveal().reveal(".home-content h3, .home-content p, .about-contet", {origin: "right"});
